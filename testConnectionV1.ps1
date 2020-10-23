c4b6c82e00b85409a4d3cb3b8d821a61a1e42b7d
Dinoyan Ganeshalingam <dinoyan.g98@gmail.com>
2020-10-23 11:07:33 -0400

    Added output for the admin

--- testConnectionV1.ps1
+++ testConnectionV1.ps1
@@ -1,6 +1,6 @@
-$list = import-csv "C:\Users\ganeshd\Desktop\coop-list.csv"
+# $list = import-csv "C:\Users\ganeshd\Desktop\coop-list.csv"
 
-$complist = "WSHR2Q433", "WSHR2Q433"
+$complist = "WSHR2Q433", "WSHR2Q423"
 
 ForEach ($computer in $complist) {
  if (Test-Connection -ComputerName "$computer" -Quiet -Count 1) {
@@ -10,7 +10,12 @@ ForEach ($computer in $complist) {
 
     robocopy C:/Users/%username%/Documents/ "C:/Users/%username%/Google Drive/ComputerData/"  /xd C:\Users\%username%\Documents\IBM /MIR /R:0 /W:0'
 
-    SCHTASKS /Create /S $computer /TN GBackupTask /TR "C:\test.bat" /SC ONLOGON /RU SYSTEM
+    SCHTASKS /Create /S $computer /TN GBackupTask /TR "C:\taskTest.bat" /SC ONLOGON /RU SYSTEM
 
+    Write-Host "Task created on $computer" -ForegroundColor Green
+ } else {
+    Write-Host "$computer is not online" -ForegroundColor Red
  }
 }
+
+CMD /c PAUSE
